c83cd72ae170ea518fd9674deec5ec1b83e38c7f
taylor-james-1179 <james.taylor.1179@mail.linnbenton.edu>
2019-06-03 11:59:15 -0700

    Finished

--- part_1.py
+++ part_1.py
@@ -1,5 +1,28 @@
 #! /usr/bin/env python3
+"""Export please_conform function."""
 
 
 def please_conform(caps):
-    pass
+    """Find least amount of directions to flip all hats."""
+    if not caps:
+        return "There is no one in line.\n"
+    directions = []
+    repeat_start = 0
+    last_cap = None
+    for index, cap in enumerate(caps):
+        if not last_cap:
+            last_cap = cap
+            continue
+        if cap != last_cap:
+            directions.append((repeat_start, index - 1))
+            repeat_start = index
+            last_cap = cap
+    directions.append((repeat_start, len(caps)-1))
+
+    str_directions = ""
+    for i in range(1, len(directions), 2):
+        str_directions += (
+            "Person in position"
+            + f"{directions[i][0]} through {directions[i][1]} please flip your cap!\n"
+        )
+    return str_directions
